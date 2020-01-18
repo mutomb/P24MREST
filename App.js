1c844b20897176ce62273613b1b7d934adb9c7e4
jeanluc <mtmjea001@myuct.ac.za>
2020-01-18 21:28:55 +0200

    .

--- App.js
+++ App.js
@@ -5,7 +5,9 @@ const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-var db = mongoose.connect('mongodb://localhost:27017/property24', {
+//const locol_uri = 'mongodb://localhost:27017/property24';
+const remote_uri = 'mongodb+srv://temp:temp@cluster0-j1vkj.mongodb.net/test?retryWrites=true&w=majority'
+var db = mongoose.connect( remote_uri, {
 	useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify:false
 }, function(err) {
 	if(err) throw err;
